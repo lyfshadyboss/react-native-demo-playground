70c4514df68f33086662990c3350be2d818ac553
liuyifeng <lyfshadyboss@gmail.com>
2016-08-14 00:03:48 +0800

    add redux devtools debugger

--- counters/src/createStore.js
+++ counters/src/createStore.js
@@ -2,17 +2,25 @@
  * Created by liuyifeng on 16/8/13.
  */
 
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Platform} from 'react-native';
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
 import thunk from 'redux-thunk';
-
+import devTools from 'remote-redux-devtools';
 import {app} from './modules';
 
-const middleware = applyMiddleware(thunk);
+const enhancer = compose(
+    applyMiddleware(thunk),
+    devTools({
+        name: Platform.OS,
+        hostname: 'localhost',
+        port: 5678
+    })
+);
 
 export default (data = {}) => {
     const rootReducer = combineReducers({
         [app.NAME]: app.reducer
     });
 
-    return createStore(rootReducer, data, middleware);
+    return createStore(rootReducer, data, enhancer);
 }
